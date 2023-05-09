a5ba98b19a5b664abb5a417c47ebd85bb8f1c6c3
alexn <alexanderlnanda@gmail.com>
2023-05-09 18:25:43 -0400

    Initial Commit

---
+++ IOS/IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp.swift
+//  IOS
+//
+//  Created by Alexander Nanda on 5/9/23.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
