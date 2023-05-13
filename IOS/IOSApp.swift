b3218818f7a1634ad09fbb7afcdb8e44a197d81b
sidereior <alexanderlnanda@gmail.com>
2023-05-13 00:18:39 -0400

    alternate way of doing firebase

--- IOS/IOSApp.swift
+++ IOS/IOSApp.swift
@@ -6,12 +6,20 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct IOSApp: App {
-    var body: some Scene {
-        WindowGroup {
-            LoginPage()
-        }
+
+    init() {
+        FirebaseApp.configure()
     }
+    
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        LoginPage()
+      }
+    }
+  }
 }
