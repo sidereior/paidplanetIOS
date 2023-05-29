3f1148f022d32df6b204a841226d5339298c72c0
sidereior <alexanderlnanda@gmail.com>
2023-05-28 22:04:28 -0400

    can't figure out tab view

--- IOS/HomeView.swift
+++ IOS/HomeView.swift
@@ -19,88 +19,91 @@ struct HomeView: View {
     }
     
     var body: some View {
-        ZStack {
-            Color(hex: "C9EAD4")
-                .ignoresSafeArea()
-            
-            VStack {
-                ZStack {
-                    Color.clear // Set the color of the background box if needed
-                    
-                    VStack {
-                        HStack {
-                            Text("PaidPlanet")
-                                .font(.custom("Avenir", size: 38))
-                                .fontWeight(.black)
-                                .kerning(0.5)
-                                .foregroundColor(Color(hex: "1B463C"))
-                                .padding(.leading, 15)
-                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            
-                            Image(systemName: "person.circle")
-                                .font(.title)
-                                .padding(.trailing, 15)
-                                .foregroundColor(Color(hex: "1B463C"))
-                        }
-                        .padding(.top, 20) // Adjust the top padding to create space from the safe area
+        VStack {
+            Spacer()
+            TabView {
+                HomeTab()
+                    .tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Home")
                     }
-                }
                 
-                Text(greeting)
-                    .font(.custom("Avenir", size: 25))
-                    .font(.title)
-                    .foregroundColor(Color(hex: "1B463C"))
-                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 15)
+                AddTab()
+                    .tabItem {
+                        Image(systemName: "plus.circle.fill")
+                        Text("Add")
+                    }
                 
-                Text(Date(), style: .date)
-                    .font(.custom("Avenir", size: 20))
-                    .font(.title)
-                    .foregroundColor(Color(hex: "1B463C"))
-                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                    .padding(.leading, 15)
-                
-                Spacer()
-                    .frame(height: 50) // Adjust the height of the spacer
-                
-                Rectangle()
-                    .fill(Color(hex: "1B463C"))
-                    .frame(width: 360, height: 330)
-                    .cornerRadius(14.0)
-                
-                Spacer()
+                ProfileTab()
+                    .tabItem {
+                        Image(systemName: "person.fill")
+                        Text("Profile")
+                    }
             }
-            
-            VStack {
-                Spacer()
-                TabView {
-                    HomeTab()
-                        .tabItem {
-                            Image(systemName: "house.fill")
-                            Text("Home")
-                        }
-                    
-                    AddTab()
-                        .tabItem {
-                            Image(systemName: "plus.circle.fill")
-                            Text("Add")
-                        }
-                    
-                    ProfileTab()
-                        .tabItem {
-                            Image(systemName: "person.fill")
-                            Text("Profile")
-                        }
-                }
-                .accentColor(Color(hex: "1B463C"))
-            }
+            .accentColor(Color(hex: "1B463C"))
         }
+            
+        
     }
     
     struct HomeTab: View {
         var body: some View {
             // Home tab content
-            Text("Home Tab")
+            
+            ZStack {
+                Color(hex: "C9EAD4")
+                    .ignoresSafeArea()
+                
+                VStack {
+                    ZStack {
+                        Color.clear // Set the color of the background box if needed
+                        
+                        VStack {
+                            HStack {
+                                Text("PaidPlanet")
+                                    .font(.custom("Avenir", size: 38))
+                                    .fontWeight(.black)
+                                    .kerning(0.5)
+                                    .foregroundColor(Color(hex: "1B463C"))
+                                    .padding(.leading, 15)
+                                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                
+                                Image(systemName: "person.circle")
+                                    .font(.title)
+                                    .padding(.trailing, 15)
+                                    .foregroundColor(Color(hex: "1B463C"))
+                            }
+                            .padding(.top, 20) // Adjust the top padding to create space from the safe area
+                        }
+                    }
+                    /*
+                     Text(greeting)
+                     .font(.custom("Avenir", size: 25))
+                     .font(.title)
+                     .foregroundColor(Color(hex: "1B463C"))
+                     .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                     .padding(.leading, 15)
+                     */
+                    
+                    Text(Date(), style: .date)
+                        .font(.custom("Avenir", size: 20))
+                        .font(.title)
+                        .foregroundColor(Color(hex: "1B463C"))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 15)
+                    
+                    Spacer()
+                        .frame(height: 50) // Adjust the height of the spacer
+                    
+                    Rectangle()
+                        .fill(Color(hex: "1B463C"))
+                        .frame(width: 360, height: 330)
+                        .cornerRadius(14.0)
+                    
+                    Spacer()
+                }
+                
+            }
         }
     }
     
