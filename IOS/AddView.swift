e18d52570885f0bd12b6a0243c68bf3ff0352995
sidereior <alexanderlnanda@gmail.com>
2023-07-01 23:30:38 -0400

    backward compatibility and icon

--- IOS/AddView.swift
+++ IOS/AddView.swift
@@ -16,96 +16,97 @@ struct AddView: View {
         ZStack {
             Color(hex: "C9EAD4")
                 .ignoresSafeArea()
-            
-            VStack{
-                
-                
+            ScrollView{
+                VStack{
+                    
+                    
                     Text("Do you own and use:")
                         .font(.custom("Avenir", size: 30))
                         .fontWeight(.bold)
                         .foregroundColor(Color(hex: "1B463C"))
-                Rectangle()
-                    .fill(Color(hex: "59DB84"))
-                    .frame(height: 500)
-                    .cornerRadius(14.0)
-                    .shadow(radius: 3, x: 0, y: 3)
-                    .padding(.horizontal, 20)
-                    .overlay(
-                        VStack(alignment: .center) { // Updated alignment to center
-                            
-                        
-                            Spacer()
-                                .frame(height: 30)
-                            
-                            Button(action: {
-                                // Handle button tap
-                            }) {
-                                Text("Solar Panels")
-                                    .font(.custom("Avenir", size: 30))
-                                    .foregroundColor(Color(hex: "1B463C"))
-                                    .fontWeight(.black)
-                                    .padding(.leading, 5)
-                                Spacer()
-                                    .frame(width: 10)
+                    Rectangle()
+                        .fill(Color(hex: "59DB84"))
+                        .frame(height: 500)
+                        .cornerRadius(14.0)
+                        .shadow(radius: 3, x: 0, y: 3)
+                        .padding(.horizontal, 20)
+                        .overlay(
+                            VStack(alignment: .center) { // Updated alignment to center
                                 
-                                Image("solar")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .cornerRadius(14.0)
-                                    .frame(width: UIScreen.main.bounds.width / 5, height: 135)
-                            }
-                            
-                            
-                            
-                            Spacer()
-                                .frame(height: 30)
-                            
-                            
-                            Button(action: {
-                                // Handle button tap
-                            }) {
-                                Text("Electric Cars")
-                                    .font(.custom("Avenir", size: 30))
-                                    .foregroundColor(Color(hex: "1B463C"))
-                                    .fontWeight(.black)
-                                    .padding(.leading, 5)
                                 
                                 Spacer()
-                                    .frame(width: 10)
+                                    .frame(height: 30)
                                 
-                                Image("car")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .cornerRadius(14.0)
-                                    .frame(width: UIScreen.main.bounds.width / 5, height: 135)
-                            }
-                            
-                            
-                            
-                            
-                            Spacer()
-                                .frame(height: 30)
-                            
-                            Button(action: {
-                                // Handle button tap
-                            }) {
-                                Text("Electric Stoves")
-                                    .font(.custom("Avenir", size: 30))
-                                    .foregroundColor(Color(hex: "1B463C"))
-                                    .fontWeight(.black)
-                                    .padding(.leading, 5)
+                                Button(action: {
+                                    // Handle button tap
+                                }) {
+                                    Text("Solar Panels")
+                                        .font(.custom("Avenir", size: 30))
+                                        .foregroundColor(Color(hex: "1B463C"))
+                                        .fontWeight(.black)
+                                        .padding(.leading, 5)
+                                    Spacer()
+                                        .frame(width: 10)
+                                    
+                                    Image("solar")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .cornerRadius(14.0)
+                                        .frame(width: UIScreen.main.bounds.width / 5, height: 135)
+                                }
+                                
+                                
                                 
                                 Spacer()
-                                    .frame(width: 10)
+                                    .frame(height: 30)
                                 
-                                Image("stove")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .cornerRadius(14.0)
-                                    .frame(width: UIScreen.main.bounds.width / 5, height: 135)
+                                
+                                Button(action: {
+                                    // Handle button tap
+                                }) {
+                                    Text("Electric Cars")
+                                        .font(.custom("Avenir", size: 30))
+                                        .foregroundColor(Color(hex: "1B463C"))
+                                        .fontWeight(.black)
+                                        .padding(.leading, 5)
+                                    
+                                    Spacer()
+                                        .frame(width: 10)
+                                    
+                                    Image("car")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .cornerRadius(14.0)
+                                        .frame(width: UIScreen.main.bounds.width / 5, height: 135)
+                                }
+                                
+                                
+                                
+                                
+                                Spacer()
+                                    .frame(height: 30)
+                                
+                                Button(action: {
+                                    // Handle button tap
+                                }) {
+                                    Text("Electric Stoves")
+                                        .font(.custom("Avenir", size: 30))
+                                        .foregroundColor(Color(hex: "1B463C"))
+                                        .fontWeight(.black)
+                                        .padding(.leading, 5)
+                                    
+                                    Spacer()
+                                        .frame(width: 10)
+                                    
+                                    Image("stove")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .cornerRadius(14.0)
+                                        .frame(width: UIScreen.main.bounds.width / 5, height: 135)
+                                }
                             }
-                        }
-                    )
+                        )
+                }
             }
         }
     }
