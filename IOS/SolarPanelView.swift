bcb556ec87f6b4302813abe468a488c1c1f017b8
sidereior <alexanderlnanda@gmail.com>
2023-07-09 16:31:53 -0400

    solar panel transaction changes

--- IOS/SolarPanelView.swift
+++ IOS/SolarPanelView.swift
@@ -43,7 +43,7 @@ struct SolarPanelView: View {
                                 .font(.custom("Avenir", size: 20))
                                 .foregroundColor(.red)
                                 .fontWeight(.bold)
-                                .padding(5)
+                                .padding(7)
                                 .background(Color.white)
                                 .cornerRadius(14)
                         }
@@ -86,7 +86,7 @@ struct SolarPanelView: View {
                                 .font(.custom("Avenir", size: 20))
                                 .foregroundColor(.blue)
                                 .fontWeight(.bold)
-                                .padding()
+                                .padding(7)
                                 .background(Color.white)
                                 .cornerRadius(14)
                         }
@@ -103,7 +103,7 @@ struct SolarPanelView: View {
                                             imagePath4: $imagePath4)
                         }
                     }
-                    
+                    /*
                     if let image = selectedImage {
                         Image(uiImage: image)
                             .resizable()
@@ -174,11 +174,74 @@ struct SolarPanelView: View {
                             .background(Color.white)
                             .cornerRadius(14)
                     }
+                     */
                 }
+                     
                 .sheet(isPresented: $isShowingImagePicker, onDismiss: uploadImage) {
                     ImagePicker(selectedImage: $selectedImage)
                 }
             }
+        }
+    }
+    
+    
+    
+    
+    
+    func uploadImage() {
+        guard let image = selectedImage,
+              let imageData = image.jpegData(compressionQuality: 0.8) else {
+            return
+        }
+        
+        let storage = Storage.storage() // Get reference to the Firebase Storage
+        let storageRef = storage.reference() // Get the root reference
+        
+        let imageName = "\(UUID().uuidString).jpg"
+        let imageRef = storageRef.child("images/\(imageName)") // Create a reference to the image file with a unique filename
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        // Upload the image data to Firebase Storage
+        let uploadTask = imageRef.putData(imageData, metadata: metadata) { (metadata, error) in
+            guard error == nil else {
+                print("Error uploading image: \(error!.localizedDescription)")
+                return
+            }
+            
+            // Image uploaded successfully
+            print("Image uploaded successfully")
+            
+            // Access the download URL for the uploaded image
+            imageRef.downloadURL { (url, error) in
+                guard let downloadURL = url else {
+                    if let error = error {
+                        print("Error getting download URL: \(error.localizedDescription)")
+                    }
+                    return
+                }
+                
+                // Use the download URL for further operations (e.g., save it to a database)
+                let urlString = downloadURL.absoluteString
+                print("Download URL: \(urlString)")
+                if currentImageNumber == 1 {
+                    imagePath1 = urlString
+                } else if currentImageNumber == 2 {
+                    imagePath2 = urlString
+                } else if currentImageNumber == 3 {
+                    imagePath3 = urlString
+                } else if currentImageNumber == 4 {
+                    imagePath4 = urlString
+                }
+            }
+        }
+        
+        // Add a progress observer to track the upload progress
+        uploadTask.observe(.progress) { snapshot in
+            guard let progress = snapshot.progress else { return }
+            let percentComplete = Double(progress.completedUnitCount) / Double(progress.totalUnitCount) * 100
+            print("Upload progress for image \(currentImageNumber): \(percentComplete)%")
         }
     }
     
@@ -205,6 +268,7 @@ struct SolarPanelView: View {
 
 
 
+//photo upload view
 struct PhotoUploadView: View {
     @Binding var firstName: String
     @Binding var lastName: String
@@ -215,90 +279,109 @@ struct PhotoUploadView: View {
     @State private var selectedImage: UIImage?
     @State private var isShowingImagePicker = false
     @State private var currentImageNumber = 1
+    @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        VStack {
-            Button(action: {
-                imagePath1 = nil
-                imagePath2 = nil
-                imagePath3 = nil
-                imagePath4 = nil
-            }) {
-                Text("Clear All Images")
-                    .font(.custom("Avenir", size: 20))
-                    .foregroundColor(.red)
-                    .fontWeight(.bold)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(14)
-            }
+        ZStack{
+            Color(hex: "1B463C")
+                .ignoresSafeArea()
             
-            Button(action: {
-                currentImageNumber = 1
-                isShowingImagePicker = true
-            }) {
-                Text("Upload Photo 1")
-                    .font(.custom("Avenir", size: 20))
-                    .foregroundColor(.blue)
-                    .fontWeight(.bold)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(14)
+            VStack {
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Text("Back")
+                            .font(.custom("Avenir", size: 20))
+                            .foregroundColor(.blue)
+                            .fontWeight(.bold)
+                            .padding(7)
+                            .background(Color.white)
+                            .cornerRadius(14)
+                    }
+                    .padding(.top, 20)
+                    .padding(.trailing, 20)
+                }
+                
+                Group{
+                    
+                    Text("Next, we need some proof of identification. Upload a picture of your drivers license, passport, or other form of ID so we can confirm your info.")
+                        .font(.custom("Avenir", size: 30))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .padding(.horizontal, 15)
+                    
+                    Button(action: {
+                        currentImageNumber = 1
+                        isShowingImagePicker = true
+                    }) {
+                        Text("Upload Identification")
+                            .font(.custom("Avenir", size: 20))
+                            .foregroundColor(.blue)
+                            .fontWeight(.bold)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(14)
+                    }
+                    
+                    Button(action: {
+                        currentImageNumber = 2
+                        isShowingImagePicker = true
+                    }) {
+                        Text("(Optional) Upload Identification 2")
+                            .font(.custom("Avenir", size: 20))
+                            .foregroundColor(.blue)
+                            .fontWeight(.bold)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(14)
+                    }
+                    
+                    Button(action: {
+                        currentImageNumber = 3
+                        isShowingImagePicker = true
+                    }) {
+                        Text("Upload Photo 3")
+                            .font(.custom("Avenir", size: 20))
+                            .foregroundColor(.blue)
+                            .fontWeight(.bold)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(14)
+                    }
+                    
+                    Button(action: {
+                        currentImageNumber = 4
+                        isShowingImagePicker = true
+                    }) {
+                        Text("Upload Photo 4")
+                            .font(.custom("Avenir", size: 20))
+                            .foregroundColor(.blue)
+                            .fontWeight(.bold)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(14)
+                    }
+                    
+                    Button(action: {
+                        // Code to confirm transaction
+                    }) {
+                        Text("Confirm Transaction")
+                            .font(.custom("Avenir", size: 20))
+                            .foregroundColor(.blue)
+                            .fontWeight(.bold)
+                            .padding()
+                            .background(Color.white)
+                            .cornerRadius(14)
+                    }
+                }
+                
+                Spacer()
             }
-            
-            Button(action: {
-                currentImageNumber = 2
-                isShowingImagePicker = true
-            }) {
-                Text("Upload Photo 2")
-                    .font(.custom("Avenir", size: 20))
-                    .foregroundColor(.blue)
-                    .fontWeight(.bold)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(14)
+            .sheet(isPresented: $isShowingImagePicker, onDismiss: uploadImage) {
+                ImagePicker(selectedImage: $selectedImage)
             }
-            
-            Button(action: {
-                currentImageNumber = 3
-                isShowingImagePicker = true
-            }) {
-                Text("Upload Photo 3")
-                    .font(.custom("Avenir", size: 20))
-                    .foregroundColor(.blue)
-                    .fontWeight(.bold)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(14)
-            }
-            
-            Button(action: {
-                currentImageNumber = 4
-                isShowingImagePicker = true
-            }) {
-                Text("Upload Photo 4")
-                    .font(.custom("Avenir", size: 20))
-                    .foregroundColor(.blue)
-                    .fontWeight(.bold)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(14)
-            }
-            
-            Button(action: {
-                // Code to confirm transaction
-            }) {
-                Text("Confirm Transaction")
-                    .font(.custom("Avenir", size: 20))
-                    .foregroundColor(.blue)
-                    .fontWeight(.bold)
-                    .padding()
-                    .background(Color.white)
-                    .cornerRadius(14)
-            }
-        }
-        .sheet(isPresented: $isShowingImagePicker, onDismiss: uploadImage) {
-            ImagePicker(selectedImage: $selectedImage)
         }
     }
     
@@ -355,6 +438,8 @@ struct PhotoUploadView: View {
         }
     }
 }
+
+
 
 struct SolarPanelView_Previews: PreviewProvider {
     static var previews: some View {
