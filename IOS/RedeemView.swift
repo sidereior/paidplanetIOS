2a2c4099fadc5df27b98d7c7a7dbf305281f79c7
sidereior <alexanderlnanda@gmail.com>
2023-09-10 18:22:55 -0400

    second confirm tab

--- IOS/RedeemView.swift
+++ IOS/RedeemView.swift
@@ -26,7 +26,7 @@ struct RedeemView: View {
     
     @State private var totalCO2AmountDollars: Double = 0.0
     
-    
+       @State private var isShowingSocialView = false
     private func fetchTransactions() {
         let db = Firestore.firestore()
         db.collection("transactions")
@@ -142,15 +142,15 @@ struct RedeemView: View {
                         }
                      
                     
-                    TextField("Enter your Venmo or Zelle username/email", text: $enteredWord)
+                    TextField("Enter your Venmo or Zelle username", text: $enteredWord)
                                 .padding(.vertical, 10)
                                 .background(Color(hex: "00653B"))
                                 .cornerRadius(14.0)
                                 .padding(.horizontal, 25)
-                                .font(.custom("Avenir", size: 20).bold())
+                                .font(.custom("Avenir", size: 12).bold())
                                 .foregroundColor(Color(hex: "F2E8CF"))
                                 .accentColor(.black)
-                                        Button(action: {
+                                Button(action: {
                                            firestoreService.addPayment(enteredWord: enteredWord) { result in
                                                 switch result {
                                                 case .success:
@@ -159,6 +159,8 @@ struct RedeemView: View {
                                                     print("Error adding payment: \(error.localizedDescription)")
                                                 }
                                             }
+                                            isShowingSocialView = true
+                                            
                                         }) {
                                             Text("Submit Payment")
                                                 .font(.custom("Avenir", size: 20))
@@ -169,18 +171,95 @@ struct RedeemView: View {
                                                 .cornerRadius(14)
                                         }
                                         
-                    
-                   //split this into seperate buttons
-                    Text("Share on socials that PaidPlanet just PaidMe! #CookingGreen! #DrivingGreen! #GreenEnergy! #GreenCreditForAll!")
-                           .font(.custom("Avenir", size: 25))
-                                   .fontWeight(.black)
-                                   .foregroundColor(Color(hex: "00653B"))
-                                   .padding(.horizontal, 35)
-                                   .padding(.top, 15)
-                       
+                                        .sheet(isPresented: $isShowingSocialView) {
+                                              RedeemView2()
+                                            }
                     
                 }
             }
         }
     }
+
+}
+
+
+struct RedeemView2: View {
+    
+    @State private var totalCO2AmountDollars: Double = 0.0
+    
+    private func fetchTransactions() {
+        let db = Firestore.firestore()
+        db.collection("transactions")
+            .addSnapshotListener { snapshot, error in
+                guard let documents = snapshot?.documents else {
+                    print("Error fetching documents: \(error?.localizedDescription ?? "Unknown error")")
+                    return
+                }
+                
+             var totalCO2: Double = 0.0
+               var transactions = documents.compactMap { document in
+                    do {
+                        let transaction = try document.data(as: Transaction.self)
+                        if(transaction.progress == "Completed")
+                        {
+                            totalCO2 += transaction.amountCO
+                        }
+                        return transaction
+                    } catch {
+                        print("Error decoding transaction: \(error.localizedDescription)")
+                        return nil
+                    }
+                }
+                totalCO2AmountDollars = totalCO2
+            }
+    }
+    
+    @Environment(\.presentationMode) var presentationMode
+    @State private var enteredWord = ""
+    
+    @State private var option1Selected = false
+    @State private var option2Selected = false
+
+    private let firestoreService = FirestoreService()
+    
+    var body: some View {
+        ZStack {
+            Color(hex: "F2E8CF")
+                .ignoresSafeArea()
+                        
+            ScrollView{
+                VStack {
+                    HStack{
+                        Spacer()
+                        
+                        Button(action: {
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Text("Cancel")
+                                .font(.custom("Avenir", size: 20))
+                                .foregroundColor(.red)
+                                .fontWeight(.bold)
+                                .padding()
+                                .background(Color(hex: "C3E8AC"))
+                                .cornerRadius(14)
+                        }
+                        .padding(.top, 20)
+                        .padding(.trailing, 20)
+                    }
+                    
+                   Text("Share on socials that you just got Paid through PaidPlanet!")
+                           .font(.custom("Avenir", size: 25))
+                                   .fontWeight(.black)
+                                   .foregroundColor(Color(hex: "00653B"))
+                                   .padding(.horizontal, 35)
+                                   .padding(.top, 15)
+                    
+                    
+                    
+                    
+                }
+            }
+        }
+    }
+
 }
