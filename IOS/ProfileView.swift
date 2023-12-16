8c29189da8cc2a937cb703e26d45b85f2583409a
sidereior <alexanderlnanda@gmail.com>
2023-12-15 22:41:09 -0500

    profile view scrolls

--- IOS/ProfileView.swift
+++ IOS/ProfileView.swift
@@ -9,122 +9,124 @@ struct ProfileView: View {
     @State private var isShowingDeleteView = false
 
     var body: some View {
-        VStack {
-            HStack {
-                Text(greeting)
-                    .fontWeight(.black)
-                    .foregroundColor(Color(hex: "7D5E35"))
-                    .font(.title)
-                    .padding()
-                    .background(Color(hex: "D1AD7D"))
-                    .cornerRadius(10)
-                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-            }
-            .padding(.bottom, 10)
-
-            Text("Email: \(userManager.user?.email ?? "")")
-                .font(.headline)
-                .fontWeight(.bold)
-                .foregroundColor(Color(hex: "C3E8AC"))
-                .padding()
-                .background(Color(hex: "00653B"))
-                .cornerRadius(10)
-                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-                .padding(10)
-
-            Button(action: {
-                isShowingResetPasswordAlert = true
-            }) {
-                Text("Change Password")
-                    .foregroundColor(Color(hex: "72cff7"))
-                    .font(.title)
+        ScrollView{
+            VStack {
+                HStack {
+                    Text(greeting)
+                        .fontWeight(.black)
+                        .foregroundColor(Color(hex: "7D5E35"))
+                        .font(.title)
+                        .padding()
+                        .background(Color(hex: "D1AD7D"))
+                        .cornerRadius(10)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                }
+                .padding(.bottom, 10)
+                
+                Text("Email: \(userManager.user?.email ?? "")")
+                    .font(.headline)
                     .fontWeight(.bold)
+                    .foregroundColor(Color(hex: "C3E8AC"))
                     .padding()
                     .background(Color(hex: "00653B"))
                     .cornerRadius(10)
                     .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-                    .padding(.bottom, 10)
+                    .padding(10)
+                
+                Button(action: {
+                    isShowingResetPasswordAlert = true
+                }) {
+                    Text("Change Password")
+                        .foregroundColor(Color(hex: "72cff7"))
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding()
+                        .background(Color(hex: "00653B"))
+                        .cornerRadius(10)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                        .padding(.bottom, 10)
+                }
+                .alert(isPresented: $isShowingResetPasswordAlert) {
+                    Alert(
+                        title: Text("Change Password"),
+                        message: Text("Are you sure you want to change your password? An email will be sent to you shortly."),
+                        primaryButton: .default(Text("Reset"), action: resetPassword),
+                        secondaryButton: .cancel()
+                    )
+                }
+                
+                Button(action: {
+                    isShowingFeedbackView = true
+                }) {
+                    Text("Leave Feedback")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.green)
+                        .padding()
+                        .background(Color(hex: "00653B"))
+                        .cornerRadius(10)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                        .padding(.bottom, 10)
+                }
+                .sheet(isPresented: $isShowingFeedbackView) {
+                    FeedbackView(isPresented: $isShowingFeedbackView)
+                }
+                
+                Button(action: {
+                    showWelcomeFrameView = true
+                }) {
+                    Text("Replay Tutorial")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.green)
+                        .padding()
+                        .background(Color(hex: "00653B"))
+                        .cornerRadius(10)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                        .padding(.bottom, 10)
+                }
+                .sheet(isPresented: $showWelcomeFrameView) {
+                    WelcomeFrameView()
+                }
+                
+                Button(action: {
+                    isShowingDeleteView = true
+                }) {
+                    Text("Delete your account")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                        .padding()
+                        .background(Color(hex: "00653B"))
+                        .cornerRadius(10)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                        .padding(.bottom, 10)
+                }
+                .sheet(isPresented: $isShowingDeleteView) {
+                    DeleteView(isPresented: $isShowingDeleteView)
+                }
+                
+                
+                Button(action: logout) {
+                    Text("Logout")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color(hex: "ff6363"))
+                        .padding()
+                        .background(Color(hex: "00653B"))
+                        .cornerRadius(10)
+                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                        .padding(.bottom, 10)
+                }
+                
+                Spacer()
+                
+                
             }
-            .alert(isPresented: $isShowingResetPasswordAlert) {
-                Alert(
-                    title: Text("Change Password"),
-                    message: Text("Are you sure you want to change your password? An email will be sent to you shortly."),
-                    primaryButton: .default(Text("Reset"), action: resetPassword),
-                    secondaryButton: .cancel()
-                )
+            .navigationBarTitle("Profile")
+            .onAppear {
+                userManager.fetchUser()
             }
-
-            Button(action: {
-                isShowingFeedbackView = true
-            }) {
-                Text("Leave Feedback")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.green)
-                    .padding()
-                    .background(Color(hex: "00653B"))
-                    .cornerRadius(10)
-                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-                    .padding(.bottom, 10)
-            }
-            .sheet(isPresented: $isShowingFeedbackView) {
-                FeedbackView(isPresented: $isShowingFeedbackView)
-            }
-               
-            Button(action: {
-                        showWelcomeFrameView = true
-            }) {
-                Text("Replay Tutorial")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.green)
-                    .padding()
-                    .background(Color(hex: "00653B"))
-                    .cornerRadius(10)
-                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-                    .padding(.bottom, 10)
-                    }
-                    .sheet(isPresented: $showWelcomeFrameView) {
-                        WelcomeFrameView()
-                    }
-            
-           Button(action: {
-                isShowingDeleteView = true
-            }) {
-                Text("Delete your account")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.red)
-                    .padding()
-                    .background(Color(hex: "00653B"))
-                    .cornerRadius(10)
-                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-                    .padding(.bottom, 10)
-            }
-            .sheet(isPresented: $isShowingDeleteView) {
-                DeleteView(isPresented: $isShowingDeleteView)
-            }
-            
-            
-            Button(action: logout) {
-                Text("Logout")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color(hex: "ff6363"))
-                    .padding()
-                    .background(Color(hex: "00653B"))
-                    .cornerRadius(10)
-                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
-                    .padding(.bottom, 10)
-            }
-            
-           Spacer()
-            
-             
-        }
-        .navigationBarTitle("Profile")
-        .onAppear {
-            userManager.fetchUser()
         }
     }
 
